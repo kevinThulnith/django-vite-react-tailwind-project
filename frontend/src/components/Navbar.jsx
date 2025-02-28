5a91d3ab91a2c181fd24cdbab19828a5926e473d
kevin <kevinthulnith@gmail.com>
2025-03-01 02:52:41 +0530

    responsive desing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,8 @@
-import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import {
+  faRightFromBracket,
+  faBars,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import tailwindLogo from "../assets/tailwind.png";
 import djangoLogo from "../assets/django.png";
@@ -10,7 +14,9 @@ import viteLogo from "../assets/vite.png";
 const Navbar = () => {
   const logos = [reactLogo, tailwindLogo, djangoLogo, viteLogo];
   const [currentLogoIndex, setCurrentLogoIndex] = useState(0);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to manage menu visibility
 
+  // TODO: Change logo every 5 seconds
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentLogoIndex((prevIndex) => (prevIndex + 1) % 4);
@@ -18,56 +24,84 @@ const Navbar = () => {
     return () => clearInterval(interval);
   }, []);
 
+  // !Function to toggle menu visibility
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <nav className="fixed top-0 left-0 z-50 bg-white shadow-md w-full py-4">
-      <div className="container mx-auto flex items-center justify-between">
-        <div className="flex items-center flex-shrink-0">
-          <img className="h-10 mr-2" src={logos[currentLogoIndex]} alt="Logo" />
+      <div className="md:container md:mx-auto mx-4 flex items-center justify-between">
+        <div className="flex">
+          <img className="h-9 mr-2" src={logos[currentLogoIndex]} alt="Logo" />
           <NavLink
             to="/"
             style={{
-              fontWeight: 800,
+              fontWeight: 700,
               color: "#3172c4",
               fontSize: "24px",
-              letterSpacing: "1px",
+              letterSpacing: "0.5px",
             }}
           >
             DRVT Project
           </NavLink>
         </div>
+
+        <button
+          onClick={toggleMenu}
+          className="md:hidden text-gray-700 outline-none border-none text-2xl"
+        >
+          <FontAwesomeIcon
+            icon={isMenuOpen ? faTimes : faBars} // Toggle between menu and close icon
+            aria-hidden="true"
+          />
+        </button>
+
         <div
-          className="space-x-4"
-          style={{ color: "#999", fontWeight: 500, opacity: 0.8 }}
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } md:flex md:space-x-4 absolute md:static bg-white w-full md:w-auto left-0 top-16 py-4 md:py-0 shadow-md md:shadow-none`}
+          style={{ color: "#999", fontWeight: 500, fontSize: "17px" }}
         >
           <NavLink
             to="/"
             className={({ isActive }) =>
-              isActive ? "text-black" : "hover:text-gray-700"
+              `block md:inline-block py-2 mx-4 md:mx-0 ${
+                isActive ? "text-black" : "hover:text-gray-700"
+              }`
             }
+            onClick={() => setIsMenuOpen(false)} // Close menu on link click
           >
             Home
           </NavLink>
           <NavLink
             to="/products"
             className={({ isActive }) =>
-              isActive ? "text-black" : "hover:text-gray-700"
+              `block md:inline-block py-2 mx-4 md:mx-0 ${
+                isActive ? "text-black" : "hover:text-gray-700"
+              }`
             }
+            onClick={() => setIsMenuOpen(false)} // Close menu on link click
           >
             My Products
           </NavLink>
           <NavLink
             to="/addProduct"
             className={({ isActive }) =>
-              isActive ? "text-black" : "hover:text-gray-700"
+              `block md:inline-block py-2 mx-4 md:mx-0 ${
+                isActive ? "text-black" : "hover:text-gray-700"
+              }`
             }
+            onClick={() => setIsMenuOpen(false)} // Close menu on link click
           >
             Add Product
           </NavLink>
           <NavLink
             to="/logout"
             className={({ isActive }) =>
-              isActive ? "text-black" : "hover:text-gray-700"
+              `block md:inline-block py-2 mx-4 md:mx-0 ${
+                isActive ? "text-black" : "hover:text-gray-700"
+              }`
             }
+            onClick={() => setIsMenuOpen(false)} // Close menu on link click
           >
             Logout
             <FontAwesomeIcon
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -22,78 +22,79 @@ function Register() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-full max-w-md bg-white rounded-lg shadow-md p-6">
-        <h2 className="text-5xl font-semibold text-center text-gray-700 pt-3">
-          Register
-        </h2>
-        <form action="" className="mt-10" onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label
-              htmlFor="username"
-              className="block text-1xl font-medium text-gray-600"
-            >
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none"
-              placeholder="Enter your username"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-1xl font-medium text-gray-600"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none"
-              placeholder="Enter your email"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-1xl font-medium text-gray-600"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
-          {loading && <LoadingIndicator />}
-          <button
-            type="submit"
-            className="w-full mt-2 px-4 py-2 text-slate-100 bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+    <div className="form-container w-[380px] bg-white rounded-lg shadow-md p-6 mx-auto">
+      <h2 className="text-5xl font-semibold text-center text-gray-700 pt-3">
+        Register
+      </h2>
+      <form action="" className="mt-10" onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label
+            htmlFor="username"
+            className="block text-1xl font-medium text-gray-600"
           >
-            Register
-          </button>
-        </form>
-        <p className="mt-5 text-1xl text-center text-gray-600">
-          Already have an account?{" "}
-          <a href="/login" className="text-blue-500 font-medium">
-            Login
-          </a>
-        </p>
-      </div>
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none"
+            placeholder="Enter your username"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-1xl font-medium text-gray-600"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none"
+            placeholder="Enter your email"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-1xl font-medium text-gray-600"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none"
+            placeholder="Enter your password"
+            required
+          />
+        </div>
+        {loading && <LoadingIndicator />}
+        <button
+          type="submit"
+          className="w-full mt-2 px-4 py-2 text-slate-100 bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
+        >
+          Register
+        </button>
+      </form>
+      <p className="mt-5 text-1xl text-center text-gray-600">
+        Already have an account?{" "}
+        <a
+          href="/login"
+          className="text-blue-500 font-medium hover:text-blue-800"
+        >
+          Login
+        </a>
+      </p>
     </div>
   );
 }
