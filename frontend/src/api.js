671ccac35c80d4226091aac6f73359e1e6148d00
kevin <kevinthulnith@gmail.com>
2025-03-03 15:44:35 +0530

    fix: simplify request interceptor in api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -9,14 +9,10 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem(ACCESS_TOKEN);
-    if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
-    }
+    if (token) config.headers["Authorization"] = `Bearer ${token}`;
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default api;
